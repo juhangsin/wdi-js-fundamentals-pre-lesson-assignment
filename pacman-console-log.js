d0773e463e4b485b7f991c805ab1aacfd2961f23
Ju-Hang Sin <juhangsin@gmail.com>
2017-06-28 19:48:48 -0400

    moved ghosts to a new file

--- pacman-console-log.js
+++ pacman-console-log.js
@@ -1,9 +1,1 @@
 console.log('Pac-Man eats ghosts for lunch.');
-var ghosts = ['Inky', 'Blinky', 'Pinky', 'Clyde'];
-
-console.log('There are ' + ghosts.length + ' ghosts.');
-console.log('Their names are: ');
-console.log(ghosts[0]);
-console.log(ghosts[1]);
-console.log(ghosts[2]);
-console.log(ghosts[3]);
