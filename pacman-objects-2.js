e3258fac3ef17af72ffd7f9c1bfbf872b8b0854d
Ju-Hang Sin <juhangsin@gmail.com>
2017-06-28 20:11:54 -0400

    if statement

--- pacman-objects-2.js
+++ pacman-objects-2.js
@@ -2,3 +2,6 @@ var ghosts = {inky: 'Cyan', blinky: 'Red', pinky: 'Pink', clyde: 'Orange' }
 console.log(ghosts);
 console.log(ghosts.inky);
 console.log(ghostos['blinky']);
+
+var ghostName = 'clyde';
+ghosts[ghostName];
